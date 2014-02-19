58830f95b1e56b10ee953c7f93d025254b35e84f
joke2k <joke2k@gmail.com>
2014-02-19 15:17:44 +0100

    add social bar

--- footer.php
+++ footer.php
@@ -54,5 +54,21 @@
 
 
 <?php wp_footer(); ?>
+
+
+<div class="addthis_bar addthis_bar_vertical addthis_bar_large" style="top:50px;left:50px;">
+    <div class="addthis_toolbox addthis_default_style">
+        <span><a class="addthis_button_facebook_like" fb:like:layout="box_count"></a></span>
+        <span><a class="addthis_button_tweet" tw:count="vertical"></a></span>
+        <span><a class="addthis_button_google_plusone" g:plusone:size="tall"></a></span>
+        <span><a class="addthis_counter"></a></span>
+    </div>
+</div>
+<script type="text/javascript">
+    var addthis_config = {"data_track_clickback":true};
+</script> <script type="text/javascript"
+                  src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-530489714d916add"></script>
+
+
 </body>
 </html>
