5fc999db4c275893c373dfcd332c8f801ea8b43a
joke2k <joke2k@gmail.com>
2014-02-19 15:23:03 +0100

    fix social bar

--- footer.php
+++ footer.php
@@ -56,18 +56,16 @@
 <?php wp_footer(); ?>
 
 
-<div class="addthis_bar addthis_bar_vertical addthis_bar_large" style="top:50px;left:50px;">
-    <div class="addthis_toolbox addthis_default_style">
-        <span><a class="addthis_button_facebook_like" fb:like:layout="box_count"></a></span>
-        <span><a class="addthis_button_tweet" tw:count="vertical"></a></span>
-        <span><a class="addthis_button_google_plusone" g:plusone:size="tall"></a></span>
-        <span><a class="addthis_counter"></a></span>
-    </div>
+<!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_floating_style addthis_counter_style" style="left:50px;top:50px;">
+    <a class="addthis_button_facebook_like" fb:like:layout="box_count"></a>
+    <a class="addthis_button_tweet" tw:count="vertical"></a>
+    <a class="addthis_button_google_plusone" g:plusone:size="tall"></a>
+    <a class="addthis_counter"></a>
 </div>
-<script type="text/javascript">
-    var addthis_config = {"data_track_clickback":true};
-</script> <script type="text/javascript"
-                  src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-530489714d916add"></script>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-530489714d916add"></script>
+<!-- AddThis Button END -->
 
 
 </body>
