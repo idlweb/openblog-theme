922082fcc99f9ec462c988b9e7fdb197029f769f
joke2k <joke2k@gmail.com>
2014-02-20 16:20:31 +0100

    edit thumb sizes to 100x100

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ $openpolis_links = array(
 
 if ( function_exists( 'add_theme_support' ) ) {
     add_theme_support( 'post-thumbnails' );
-    set_post_thumbnail_size( 130, 130 );
+    set_post_thumbnail_size( 100, 100 );
 }
 
 function _icon($name) {
